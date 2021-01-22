d6b57d966d8601ad50762a2fda674a7f273ed462
kittiphum <kittiphum_s@cmu.ac.th>
2021-01-22 17:17:59 +0700

    เสร็จ

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int j = 1;j < N;j++){
+		T temp = d[j];
+		if(temp > d[j-1]){
+			for(int i = j-1;i >= 0;i--){
+				d[i+1] = d[i];
+				if(temp <= d[i-1] or i == 0){
+					d[i] = temp;
+					break;
+				}
+			}
+		}
+		cout << "Pass " << j << ":";
+		for(int k = 0;k < N;k++){
+			cout << d[k] << " ";
+		}
+		cout << endl;
+	}
 }
 
 int main(){
@@ -17,3 +33,4 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
