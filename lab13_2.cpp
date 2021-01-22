fc2b9b534f81fbdd0fe367771f03ff1b35bd9720
kittiphum <kittiphum_s@cmu.ac.th>
2021-01-22 17:26:34 +0700

    เสร็จ

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -19,3 +19,38 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double A[N][N]){
+	int x = 1;
+	for(int i = 0;i < N;i++){
+		cout << "Row " << x << ": ";
+		for(int j = 0;j < N;j++){
+			cin >> A[i][j];
+		}
+		x++;
+	}
+}
+
+void findLocalMax(const double x[][N], bool y[][N]){
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			y[i][j] = 0;
+		}
+	}
+	for (int i = 1; i < N-1; i++){
+		for (int j = 1; j < N-1; j++){
+			if (x[i][j] >= x[i - 1][j] && x[i][j] >= x[i][j + 1] && x[i][j] >= x[i + 1][j] && x[i][j] >= x[i][j - 1]){
+				y[i][j] = 1;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool A[N][N]){
+	for(int i = 0;i < N;i++){
+		for(int j = 0;j < N;j++){
+			cout << A[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
